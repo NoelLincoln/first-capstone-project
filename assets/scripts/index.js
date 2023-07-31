3d7d28b478fce7e45bcf4d7599dbc0042ab9daa7
Noel Bryant <noelsobryant@gmail.com>
2023-07-31 10:31:09 +0300

    add navbar functionality

---
+++ assets/scripts/index.js
@@ -0,0 +1,24 @@
+const menuicon = document.getElementById('menu');
+const navlinks = document.getElementById('nav-links');
+const desktop = document.querySelector('.desktop');
+const menuclose = document.getElementById('menu-close');
+
+function menuOverlay() {
+  if (menuicon.style.display === 'flex') {
+    menuicon.style.display = 'none';
+    menumodal.style.display = 'flex';
+  } else {
+    menuicon.style.display = 'none';
+    menuclose.style.display = 'block';
+    navlinks.style.display = 'flex';
+  }
+}
+menuicon.addEventListener('click', menuOverlay);
+
+function closeMenuOverlay() {
+  navlinks.style.display = 'none';
+  menuicon.style.display = 'flex';
+  menuclose.style.display = 'none';
+}
+
+menuclose.addEventListener('click', closeMenuOverlay);
